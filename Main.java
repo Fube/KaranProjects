d3410f64bd3835b9458dca937137b2abf2233584
Fube <fubeitch@gmail.com>
2020-08-01 22:13:40 -0400

    Added E to the Nth Digit

--- Main.java
+++ Main.java
@@ -24,9 +24,29 @@ public class Main {
 
             return new BigDecimal(bar.multiply(foo).toBigInteger()).divide(foo);
         }
+
+        /**
+         * Find E to the Nth Digit:
+         * Enter a number and have the program generate E up to that many decimal places.
+         * Keep a limit to how far the program will go.
+         *
+         * @param   lim Limits decimal places (lim <= 50)
+         * @return  E limited to lim decimal places
+         */
+        static BigDecimal eToTheNthDigit(int lim){
+            if(lim > 50)
+                throw new IllegalArgumentException("lim must be lower than or equal to 50");
+
+            BigDecimal  foo = BigDecimal.TEN.pow(lim),
+                    bar = new BigDecimal("2.71828182845904523536028747135266249775724709369995957496696762772407663");
+            return new BigDecimal(bar.multiply(foo).toBigInteger()).divide(foo);
+        }
     }
+
+
 
     public static void main(String[] args) {
         System.out.println(Numbers.piToTheNthDigit(49));
+        System.out.println(Numbers.eToTheNthDigit(2));
     }
 }
